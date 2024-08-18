f99e2c169f549c4c4c57fcdecd460192daa42a8d
Karthikayan <skarthikayan7@gmail.com>
2024-08-18 23:29:52 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "triptreasure",
+  base: "/triptreasure",
   plugins: [
     react(),
     svgr({
